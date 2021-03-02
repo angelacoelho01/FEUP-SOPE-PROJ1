1873e9ecc722322fa642dfcb4e77dc04f421a8e7
Ana Matilde Guedes Perez da Silva Barra <up201904795@fe.up.pt>
2021-03-02 12:38:22 +0000

    Added Arguments to main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-       	printf("Hello world!\n");
+int main(int numArg, char *argv[], char *env[]){
+
+	printf("Print all the arguments\n");
+
+	for (int i = 0; i < numArg; i++){
+		printf(" %d - %s\n", i, argv[i]);
+	}
+       	
 	exit(0);
 }
