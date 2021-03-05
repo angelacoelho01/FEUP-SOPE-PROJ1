b14b1a019833947be8a8fdd874d2f1bf4140ea6a
Ângela Coelho <62039496+angelacoelho01@users.noreply.github.com>
2021-03-05 13:56:55 +0000

    Encapsulate strcat calls

--- src/filesystem/filesystem.c
+++ src/filesystem/filesystem.c
@@ -54,10 +54,7 @@ int iterate_directory(const char *dirpath, bool iterate_sub_dirs) {
 			
 			// build the path from the directory (--> change to not depend of the lenght <--)
 			char path[80] = ""; char separator = '/';
-			//strcat(strncat(strcat(path, dirpath), &separator, 1), dir->d_name);
-			strcat(path, dirpath);
-			strcat(path, &separator);
-			strcat(path, dir->d_name);
+			strcat(strncat(strcat(path, dirpath), &separator), dir->d_name);
 			
 			// its a directory  
 			if (is_path_dir(path)) {
