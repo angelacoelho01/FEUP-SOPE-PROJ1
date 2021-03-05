9137d2b99ea757eb942fb52d037f97cd30a4e881
Ângela Coelho <62039496+angelacoelho01@users.noreply.github.com>
2021-03-05 13:41:15 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,12 +14,12 @@ int main(int numArg, char *argv[], char *env[]){
 	
 	if (is_path_dir(root)) {
 		// the value of this bool represents if -R option exists
-		bool change_subdirectories = true;
+		//bool change_subdirectories = true;
 		
 		printf("Is a directory! \n"); // to test purposes
 		// with -R option we need to recursevely change the permissions 
 		// of every file/dir within the directory, and other subdirectories that may exist
-		if (iterate_directory(root, change_subdirectories) == -1) {
+		if (iterate_directory(root, true) == -1) {
 			fprintf(stderr, "Error changing dir's files permissions \n");
 			exit(EXIT_FAILURE);
 		}
