112af99d9b7ff4c232ae99c294b3ee5ce05a0c9c
Angela Coelho <angela.01.coelho@gmail.com>
2021-03-07 03:06:36 +0000

    Attemp to improve code quality in convertModeString function

--- src/utils.c
+++ src/utils.c
@@ -13,18 +13,19 @@ char* convertModeToString(const mode_t mode){
 	const int read[] = {S_IRUSR, S_IRGRP, S_IROTH};
 	const int write[] = {S_IWUSR, S_IWGRP, S_IWOTH};
 	const int execute[] = {S_IXUSR, S_IXGRP, S_IXOTH};
-    char* perm = (char*)malloc(MAX_STR_LEN);
+    char* str_mode = (char*)malloc(MAX_STR_LEN);
+    memset(str_mode, 0, MAX_STR_LEN);
 
 	for(int i = 0; i < n; i++){
-		if(mode & read[i]) strcat(perm, "r");
-		else strcat(perm, "-");
+		if(mode & read[i]) strcat(str_mode, "r");
+		else strcat(str_mode, "-");
 
-		if(mode & write[i]) strcat(perm, "w");
-		else strcat(perm, "-");
+		if(mode & write[i]) strcat(str_mode, "w");
+		else strcat(str_mode, "-");
 
-		if(mode & execute[i]) strcat(perm, "x");
-		else strcat(perm, "-");
+		if(mode & execute[i]) strcat(str_mode, "x");
+		else strcat(str_mode, "-");
 	}
 
-    return perm;
+    return str_mode;
 }
